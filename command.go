d2830824ec84732330f30c5eac19e6062dd36ae6
Matt Martz <matt@sivel.net>
2015-02-12 15:27:35 -0600

    Increase gomaxprocs to number of CPUs

--- command.go
+++ command.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"runtime"
+
 	"github.com/sivel/overseer/config"
 	"github.com/sivel/overseer/runner"
 )
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 func main() {
 	monitors, notifiers := config.ParseConfig()
